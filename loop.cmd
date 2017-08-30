4f9c35dfe53cf946bbfdc0eb07809be6623d9d0f
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2017-08-30 12:28:02 -0700

    update loop.cmd

--- loop.cmd
+++ loop.cmd
@@ -1,15 +1,18 @@
 pushd ..\idembot
 call git pull
+call npm update
 call npm build
 popd
 
 pushd ..\definitelytyped-header-parser
 call git pull
+call npm update
 call npm build
 popd
 
 call git pull
 call npm update
+call npm build
 call npm run wet
 timeout 3600
 loop
