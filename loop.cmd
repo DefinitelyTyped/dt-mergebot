157c1a3eef67e9bdd24c7b5f1837b036e0f4eb32
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2017-09-07 10:32:35 -0700

    Fix loop

--- loop.cmd
+++ loop.cmd
@@ -2,16 +2,21 @@ pushd ..\idembot
 call git pull
 call npm update
 call npm build
+call npm link
 popd
 
 pushd ..\definitelytyped-header-parser
 call git pull
 call npm update
 call npm build
+call npm link
 popd
+
 
 call git pull
 call npm update
+call npm link idembot
+call npm link definitelytyped-header-parser
 call npm build
 call npm run wet
 timeout 3600
