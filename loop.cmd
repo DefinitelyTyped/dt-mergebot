a8b295e7228c0c5509bd69538b65633d963508ce
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2017-12-27 13:00:05 -0800

    Simplify loop

--- loop.cmd
+++ loop.cmd
@@ -1,25 +1,3 @@
-pushd ..\idembot
-call git checkout -- *
-call git pull
-call npm update
-call npm run build
-call npm link
-popd
-
-pushd ..\definitelytyped-header-parser
-call git checkout -- *
-call git pull
-call npm update
-call npm run build
-call npm link
-popd
-
-
-call git checkout -- *
-call git pull
-call npm update
-call npm link idembot
-call npm link definitelytyped-header-parser
 call npm run build
 call npm run wet
 call npm run cleanproject
