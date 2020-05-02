b375f61ea79e0458437383daa2c157bf9ab8a6f8
Orta Therox <orta.therox@gmail.com>
2020-05-02 10:45:28 -0400

    Improve warnings when infra files are changed

--- src/compute-pr-actions.ts
+++ src/compute-pr-actions.ts
@@ -339,6 +339,10 @@ function createWelcomeComment(info: PrInfo) {
         introCommentLines.push(` * ${emoji(approval.approved)} Most recent commit is approved by ${approval.requiredApprovalBy}`);
     } else if (otherOwners.length === 0) { 
         introCommentLines.push(` * ${emoji(approval.approved)} A DT maintainer can merge changes when there are no other reviewers`);
+    } else if (info.files.find(f => f.kind === "infrastructure")) { 
+        const infraFiles = info.files.filter(f => f.kind === "infrastructure")
+        const links = infraFiles.map(f => `[${f.filePath}](https://github.com/DefinitelyTyped/DefinitelyTyped/${info.headCommitOid}/${f})`)
+        introCommentLines.push(` * ${emoji(approval.approved)} A DT maintainer needs to merge changes which affect DT infrastructure (${links.join(", ")})`);
     } else if (info.dangerLevel === "ScopedAndConfiguration") { 
         introCommentLines.push(` * ${emoji(approval.approved)} A DT maintainer needs to merge changes which affect module config files`);
     } else {
