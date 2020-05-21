82a220cce08c01d4a61f12aff585faa4a906ac2c
Orta Therox <orta.therox@gmail.com>
2020-05-21 13:39:26 -0400

    Add a concept of side-effects which allows for auto-merging the code-owners PR hwne it is green

---
+++ src/side-effects/merge-codeowner-prs.ts
@@ -0,0 +1,27 @@
+import { WebhookPayloadCheckSuite } from "@octokit/webhooks";
+import { createMutation, mutate } from "../graphql-client";
+import { mergePr } from "../execute-pr-actions";
+
+export const mergeCodeOwnersOnGreen = async (payload: WebhookPayloadCheckSuite) => {
+  // Because we only care about GH actions, we can use the check suite API which means we get both the
+  // commit and the PR, making it much less effort than other merge-on-greens
+  // https://github.com/microsoft/TypeScript-repos-automation/blob/40ae8b3db63fd0150938e82e47dcb63ce65f7a2d/src/checks/mergeOnGreen.ts#L1
+
+  if (payload.action !== "completed") return;
+
+  const isGreen = payload.check_suite.conclusion === "success";
+  const isFromBot = payload.check_suite.head_commit.author.name === "TS Bot";
+  const hasRightCommitMsg = payload.check_suite.head_commit.message === "Update CODEOWNERS";
+  const isFromSameRepo = payload.check_suite.pull_requests[0].base.repo.id === payload.check_suite.pull_requests[0].head.repo.id;
+
+  if (isGreen && isFromBot && hasRightCommitMsg && isFromSameRepo) {
+    const mergeMutation = createMutation(mergePr, {
+      commitHeadline: `🤖 Auto Merge`,
+      expectedHeadOid: payload.check_suite.head_commit.id,
+      mergeMethod: "SQUASH",
+      pullRequestId: payload.check_suite.pull_requests[0].id,
+    });
+
+    await mutate(mergeMutation);
+  }
+};
