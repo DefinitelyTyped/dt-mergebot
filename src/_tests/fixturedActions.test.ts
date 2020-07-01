4ed7c741a5221c39b901254419c856d136332387
Eli Barzilay <eli@barzilay.org>
2020-07-01 10:56:38 -0400

    `_tests/fixturedActions.test.ts`: lint, reorganize, tweak

--- src/_tests/fixturedActions.test.ts
+++ src/_tests/fixturedActions.test.ts
@@ -1,63 +1,62 @@
-import { readdirSync, readFileSync, lstatSync } from 'fs'
-import { join } from 'path'
-import { toMatchFile } from 'jest-file-snapshot'
-import { process } from '../compute-pr-actions'
-import { deriveStateForPR, PrInfo } from '../pr-info'
-import * as cachedQueries from './cachedQueries.json';
+import { readdirSync, readFileSync, lstatSync } from "fs";
+import { join } from "path";
+import { toMatchFile } from "jest-file-snapshot";
+import { process } from "../compute-pr-actions";
+import { deriveStateForPR, PrInfo } from "../pr-info";
+import * as cachedQueries from "./cachedQueries.json";
 jest.mock("../util/cachedQueries", () => ({
   getProjectBoardColumns: jest.fn(() => cachedQueries.getProjectBoardColumns),
   getLabels: jest.fn(() => cachedQueries.getLabels)
-}))
-import { executePrActions } from '../execute-pr-actions';
+}));
+import { executePrActions } from "../execute-pr-actions";
 
-expect.extend({ toMatchFile })
+expect.extend({ toMatchFile });
 
 /* You can use the following command to add/update fixtures with an existing PR
-  
-  env BOT_AUTH_TOKEN=XYZ npm run create-fixture -- 43164
+ *
+ *     BOT_AUTH_TOKEN=XYZ npm run create-fixture -- 43164
+ */
 
-.*/
+async function testFixture(dir: string) {
+  // _foo.json are input files, except for Date.now from derived.json
+  const responsePath = join(dir, "_response.json");
+  const ownersPath = join(dir, "_owners.json");
+  const downloadsPath = join(dir, "_downloads.json");
+  const derivedPath = join(dir, "derived.json");
+  const resultPath = join(dir, "result.json");
+  const mutationsPath = join(dir, "mutations.json");
 
-// TODO: The process function uses date.now and we will need to mock that per test
+  const readJSON = (file: string) => JSON.parse(readFileSync(file, "utf8"));
+  const JSONString = (value: any) => JSON.stringify(value, null, "  ");
 
-describe('with fixtures', () => {
-  const fixturesFolder = join(__dirname, 'fixtures')
+  const response = readJSON(responsePath);
 
+  // Because Owners is another API call, we need to make it a fixture also
+  // and so this fixture overrides the current
+  const derived = await deriveStateForPR(
+    response,
+    () => readJSON(ownersPath),
+    () => readJSON(downloadsPath),
+    () => new Date(readJSON(derivedPath).now)
+  );
+
+  if (derived.type === "fail") throw new Error("Should never happen");
+
+  const action = process(derived);
+
+  expect(JSONString(action)).toMatchFile(resultPath);
+  expect(JSONString(derived)).toMatchFile(derivedPath);
+
+  const mutations = await executePrActions(action, response.data, /*dry*/ true);
+  expect(JSONString(mutations.map(m => JSON.parse(m)))).toMatchFile(mutationsPath);
+}
+
+describe("Test fixtures", () => {
+  const fixturesFolder = join(__dirname, "fixtures");
   readdirSync(fixturesFolder).forEach(fixtureName => {
-    const fixture = join(fixturesFolder, fixtureName)
-    if (!lstatSync(fixture).isDirectory()) {
-      return
+    const fixture = join(fixturesFolder, fixtureName);
+    if (lstatSync(fixture).isDirectory()) {
+      it(`Fixture: ${fixtureName}`, async () => testFixture(fixture));
     }
-
-    it('Fixture: ' + fixtureName, async () => {
-      const responseJSONPath = join(fixture, "_response.json")
-      const ownersJSONPath = join(fixture, "_owners.json")
-      const downloadsJSONPath = join(fixture, "_downloads.json")
-      const derivedJSONPath = join(fixture, "derived.json")
-      const actionJSONPath = join(fixture, "result.json")
-      const mutationsPath = join(fixture, "mutations.json")
-
-      const response = JSON.parse(readFileSync(responseJSONPath, "utf8"))
-      const existingDerivedJSON: PrInfo = JSON.parse(readFileSync(derivedJSONPath, "utf8"))
-
-      // Because Owners is another API call, we need to make it a fixture also
-      // and so this fixture overrides the current
-      const derived = await deriveStateForPR(
-        response,
-        () => JSON.parse(readFileSync(ownersJSONPath, "utf8")),
-        () => JSON.parse(readFileSync(downloadsJSONPath, "utf8")),
-        () => new Date(existingDerivedJSON.now)
-      )
-
-      if (derived.type === "fail") throw new Error("Should never happen")
-
-      const action = process(derived)
-
-      expect(JSON.stringify(action, null, "  ")).toMatchFile(actionJSONPath)
-      expect(JSON.stringify(derived, null, "  ")).toMatchFile(derivedJSONPath)
-
-      const mutations = await executePrActions(action, response.data, /*dry*/ true)
-      expect(JSON.stringify(mutations.map(m => JSON.parse(m)), null, "  ")).toMatchFile(mutationsPath)
-    })
-  })
-})
+  });
+});
