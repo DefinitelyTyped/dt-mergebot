b9564b21d2d2e9268fd082713c1a7d88749f1afe
Eli Barzilay <eli@barzilay.org>
2020-07-11 19:18:45 -0400

    Update cache and improve updating
    * Add a comment to the cache file saying how to update it.
    
    * Make it search up for the repo directory, since otherwise it would
      sometimes produce the output in the wrong place.

--- src/commands/update-test-data.ts
+++ src/commands/update-test-data.ts
@@ -3,14 +3,23 @@ import * as path from "path";
 import * as cachedQueries from "../util/cachedQueries";
 
 async function main() {
-  const dataPath = path.resolve(__dirname, "../_tests/cachedQueries.json");
+  let base = __dirname, dataPath = "";
+  while (!fs.existsSync(dataPath = path.join(base, "src", "_tests", "cachedQueries.json"))) {
+    const up = path.dirname(base);
+    if (up === base) throw new Error("Couldn't find cachedQueries.json");
+    base = up;
+  }
+
   const data: any = {};
 
   for (const query in cachedQueries) {
     data[query] = await cachedQueries[query as keyof typeof cachedQueries]();
-  }
+  };
 
-  await fs.promises.writeFile(dataPath, JSON.stringify(data, undefined, 2), 'utf8');
+  await fs.promises.writeFile(
+      dataPath,
+      JSON.stringify({comment: "Generate & update with `npm run update-test-data`", ...data},
+                     undefined, 2), 'utf8');
 }
 
 
