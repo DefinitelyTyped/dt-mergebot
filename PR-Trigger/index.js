dfa02c77e4e2cc520f3f0a217e9601bf7c04d9bf
Eli Barzilay <eli@barzilay.org>
2020-07-12 19:49:24 -0400

    More work related to the trigger handler
    * Make it handle `project_card` events with `moved` action (hacked from
      `project_card.content_url`).
    
    * Rename `ForStatus` in all its forms to `ForSHA1` since that's what it
      takes, and some ObLint.
    
    * Improved query to get the PR number: use `associatedPullRequests` from
      an `object` instead of a generic search
      ([mentioned here](https://developer.github.com/v4/changelog/2019-03-08-schema-changes/)).
    
      **No, scratch that...**  Looks like this fails *sometimes*, whereas
      the search work -- so use the old query, but keep the new one in a
      comment for reference when it works (or when there's a straightforward
      way to tell where the status came from).

--- PR-Trigger/index.js
+++ PR-Trigger/index.js
@@ -1,18 +1,18 @@
 // @ts-check
 
-const { queryPRInfo, deriveStateForPR } = require("../bin/pr-info")
-const compute = require("../bin/compute-pr-actions")
-const {executePrActions} = require("../bin/execute-pr-actions")
-const {mergeCodeOwnersOnGreen} = require("../bin/side-effects/merge-codeowner-prs")
+const { queryPRInfo, deriveStateForPR } = require("../bin/pr-info");
+const compute = require("../bin/compute-pr-actions");
+const { executePrActions } = require("../bin/execute-pr-actions");
+const { mergeCodeOwnersOnGreen } = require("../bin/side-effects/merge-codeowner-prs");
 const verify = require("@octokit/webhooks/verify");
 const sign = require("@octokit/webhooks/sign");
-const {runQueryToGetPRMetadataForStatus} = require("../bin/queries/status-to-PR-query")
+const { runQueryToGetPRMetadataForSHA1 } = require("../bin/queries/SHA1-to-PR-query");
 
 /** @type {import("@azure/functions").AzureFunction} */
 const httpTrigger = async function (context, _req) {
 
     /** @type {import("@azure/functions").HttpRequest} */
-    const req = _req
+    const req = _req;
 
     const result = process.env["BOT_AUTH_TOKEN"] || process.env["AUTH_TOKEN"];
     if (typeof result !== 'string') {
@@ -20,11 +20,11 @@ const httpTrigger = async function (context, _req) {
     }
 
     context.log('HTTP trigger function received a request.');
-    const event = req.headers["x-github-event"]
+    const event = req.headers["x-github-event"];
 
     const isDev = process.env.AZURE_FUNCTIONS_ENVIRONMENT === "Development";
     const secret = process.env.GITHUB_WEBHOOK_SECRET;
-  
+
     // For process.env.GITHUB_WEBHOOK_SECRET see
     // https://ms.portal.azure.com/#blade/WebsitesExtension/FunctionsIFrameBlade/id/%2Fsubscriptions%2F57bfeeed-c34a-4ffd-a06b-ccff27ac91b8%2FresourceGroups%2Fdtmergebot%2Fproviders%2FMicrosoft.Web%2Fsites%2FDTMergeBot
     if (!isDev && !verify(secret, req.body, sign(secret, req.body))) {
@@ -41,99 +41,103 @@ const httpTrigger = async function (context, _req) {
         "check_suite": mergeCodeOwnersOnGreen
     }
 
-    if (sideEffects[event]) sideEffects[event](req.body)
+    if (sideEffects[event]) sideEffects[event](req.body);
 
     // https://developer.github.com/webhooks/
     const acceptedEventsToActions = {
         "pull_request": ["opened", "closed", "reopened", "edited", "synchronized", "ready_for_review"],
         "pull_request_review": ["submitted", "dismissed"],
         "issue_comment": ["created", "edited", "deleted"],
+        "project_card": ["moved"],
         "check_suite": ["completed"]
     }
 
-    const acceptedEvents = Object.keys(acceptedEventsToActions)
+    const acceptedEvents = Object.keys(acceptedEventsToActions);
 
     // Bail if not a PR
     if (!acceptedEvents.includes(event)) {
-        context.log.info(`Skipped webhook ${event}, do not know how to handle the event - accepts: ${acceptedEvents.join(", ")}`)
+        context.log.info(`Skipped webhook ${event}, do not know how to handle the event - accepts: ${acceptedEvents.join(", ")}`);
         context.res = {
             status: 204,
             body: "NOOPing due to unknown event"
-        }
-        return
+        };
+        return;
     }
-    
+
     /** @type {import("@octokit/webhooks").WebhookPayloadPullRequest | import("@octokit/webhooks").WebhookPayloadPullRequestReview | import("@octokit/webhooks").WebhookPayloadIssueComment | import("@octokit/webhooks").WebhookPayloadCheckSuite } */
-    const webhook = req.body
-    const action = "action" in webhook ? webhook.action : "status"
+    const webhook = req.body;
+    const action = "action" in webhook ? webhook.action : "status";
 
     if (webhook.sender.login === "typescript-bot") {
-        context.log.info(`Skipped webhook because it was triggered by typescript-bot`)
+        context.log.info(`Skipped webhook because it was triggered by typescript-bot`);
         context.res = {
             status: 204,
             body: `NOOPing because typescript-bot triggered the request`
-        }
-        return
+        };
+        return;
     }
 
-    const allowListedActions = acceptedEventsToActions[event]
+    const allowListedActions = acceptedEventsToActions[event];
     if (!allowListedActions.includes(action) && !allowListedActions.includes("*")) {
-        context.log.info(`Skipped webhook, ${action} on ${event}, do not know how to handle the action`)
+        context.log.info(`Skipped webhook, ${action} on ${event}, do not know how to handle the action`);
         context.res = {
             status: 204,
             body: `NOOPing due to not supporting ${action} on ${event}`
         }
-        return
+        return;
     }
 
-    let prNumber = -1
-    let prTitle = ""
+    let prNumber = -1;
+    let prTitle = "(title not fetched)"; // this is only used for logging, not worth an API lookup => not always set
     if ("pull_request" in webhook) {
-        prNumber = webhook.pull_request.number
-        prTitle = webhook.pull_request.title
-    } else if("issue" in webhook) {
-        prNumber = webhook.issue.number
-        prTitle = webhook.issue.title
-    } else if("check_suite" in webhook) {
+        prNumber = webhook.pull_request.number;
+        prTitle = webhook.pull_request.title;
+    } else if ("issue" in webhook) {
+        prNumber = webhook.issue.number;
+        prTitle = webhook.issue.title;
+    } else if ("project_card" in webhook) {
+        // Hack to get the PR number, could be found directly only in `content_url`
+        const cardURL = webhook.project_card.content_url;
+        const numberInURL = cardURL.match(/\/\d+$/);
+        if (!numberInURL) throw new Error(`Could not get PR for project card URL: ${cardURL}`);
+        prNumber = +numberInURL[0].substring(1)
+    } else if ("check_suite" in webhook) {
         // See https://github.com/maintainers/early-access-feedback/issues/114 for more context on getting a PR from a SHA
         // TLDR: it's not in the API, and this search hack has been in used on Peril for the last ~3 years
-        const repoString = webhook.repository.full_name
-        const sha = webhook.check_suite.head_sha
-        const query = `${sha} type:pr  repo:${repoString}` 
-        const pr = await runQueryToGetPRMetadataForStatus(query)
-        
-        if (!pr) throw new Error(`Could not get PR for the status on ${sha} - made a search query with ${query}`)
+        // => Instead of an arbitrary search, use `associatedPullRequests` (https://developer.github.com/v4/changelog/2019-03-08-schema-changes/)
+        const owner = webhook.repository.owner.login;
+        const repo = webhook.repository.name;
+        const sha = webhook.check_suite.head_sha;
+        const pr = await runQueryToGetPRMetadataForSHA1(owner, repo, sha);
+        if (!pr) throw new Error(`Could not get PR for the status on ${sha}`);
         if (pr.closed) {
-            context.log.info(`Skipped webhook, could not find an open PR for the sha referenced in the status (${sha})`)
+            context.log.info(`Skipped webhook, could not find an open PR for the sha referenced in the status (${sha})`);
             context.res = {
                 status: 204,
                 body: `NOOPing due to not finding an open PR for the sha ${sha}`
-            }
+            };
         }
-
-        prNumber = pr.number
-        prTitle = "" // this is only used for logging, not worth an API lookup
+        prNumber = pr.number;
     }
-    
-    if (prNumber === -1) throw new Error(`PR Number was not set from a webhook - ${event} on ${action}`)
 
-    context.log.info(`Getting info for PR ${prNumber} - ${prTitle}`)
+    if (prNumber === -1) throw new Error(`PR Number was not set from a webhook - ${event} on ${action}`);
+
+    context.log.info(`Getting info for PR ${prNumber} - ${prTitle}`);
 
     // Generate the info for the PR from scratch
-    const info = await queryPRInfo(prNumber)
-    const state = await deriveStateForPR(info)
-    
+    const info = await queryPRInfo(prNumber);
+    const state = await deriveStateForPR(info);
+
     // If it didn't work, bail early
     if (state.type === "fail") {
-        const isIssueNotPR = state.message === "No PR with this number exists" && "issue" in webhook
+        const isIssueNotPR = state.message === "No PR with this number exists" && "issue" in webhook;
         if (isIssueNotPR) {
             context.res = {
                 status: 204,
                 body: `NOOPing due to ${prNumber} not being a PR`
             };
         } else {
-            context.log.error(`Failed because of: ${state.message}`)
-            
+            context.log.error(`Failed because of: ${state.message}`);
             context.res = {
                 status: 422,
                 body: `Failed because of: ${state.message}`
@@ -145,18 +149,17 @@ const httpTrigger = async function (context, _req) {
 
     // Allow the state to declare that nothing should happen
     if (state.type === "noop") {
-        context.log.info(`NOOPing because of: ${state.message}`)
-            
+        context.log.info(`NOOPing because of: ${state.message}`);
         context.res = {
             status: 204,
             body: `NOOPing because of: ${state.message}`
         };
-        return
+        return;
     }
 
     // Convert the info to a set of actions for the bot
     const actions = compute.process(state);
-    
+
     // Act on the actions
     await executePrActions(actions, info.data);
 
@@ -164,7 +167,7 @@ const httpTrigger = async function (context, _req) {
     // as a timeout in GH a few times (e.g. after GH/DT/NPM lookups)
     context.res = {
         status: 200,
-        body: actions 
+        body: actions
     };
 };
 
---
+++ src/queries/SHA1-to-PR-query.ts
@@ -0,0 +1,45 @@
+import { gql } from "apollo-boost";
+import { client } from "../graphql-client"
+import { GetPRForSHA1 } from "./schema/GetPRForSHA1"
+
+export const runQueryToGetPRMetadataForSHA1 = async (owner: string, repo: string, sha1: string) => {
+  const info = await client.query<GetPRForSHA1>({
+      query: GetPRForSHA1Query,
+      variables: { query: `${sha1} type:pr repo:${owner}/${repo}` },
+      fetchPolicy: "network-only",
+      fetchResults: true
+  });
+  const pr = info.data.search.nodes?.[0];
+  return pr?.__typename === "PullRequest" ? pr : undefined;
+}
+
+export const GetPRForSHA1Query = gql`
+query GetPRForSHA1($query: String!) {
+  search(query: $query, first: 1, type: ISSUE) {
+    nodes {
+      ... on PullRequest {
+        title
+        number
+        closed
+      }
+    }
+  }
+}`;
+
+/* This is better since it doesn't do a generic search, but for some reason it will sometime fail to get a PR
+query GetPRForSHA1($owner: String!, $repo: String!, $sha1: String!) {
+  repository(owner: $owner, name: $repo) {
+    object(expression: $sha1) {
+      ... on Commit {
+        associatedPullRequests(first: 1) {
+          nodes {
+            title
+            number
+            closed
+          }
+        }
+      }
+    }
+  }
+}
+*/
