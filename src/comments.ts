a1dd693592c80527f4a2f631b28cc8ddb8591e67
Eli Barzilay <eli@barzilay.org>
2020-12-07 07:28:23 -0500

    Don't suggest dismissing reviewes
    Fixes #253.

--- src/comments.ts
+++ src/comments.ts
@@ -28,7 +28,7 @@ export const MergeConflicted = (slug: string, user: string) => ({
 
 export const ChangesRequest = (headOid: string, user: string) => ({
     tag: `reviewer-complaint-${headOid}`,
-    status: `@${user} One or more reviewers has requested changes. Please address their comments. I'll be back once they sign off or you've pushed new commits or comments. If you disagree with the reviewer's comments, you can "dismiss" the review using GitHub's review UI. Thank you!`
+    status: `@${user} One or more reviewers has requested changes. Please address their comments. I'll be back once they sign off or you've pushed new commits. Thank you!`
 });
 
 export const PingReviewers = (names: readonly string[], reviewLink: string) => ({
