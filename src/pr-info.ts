a30b4493ba6082ffe7c0824e9fe854a8a0bd0e00
Andrew Branch <andrew@wheream.io>
2021-01-06 14:44:22 -0800

    Prevent breaking old tests

--- src/pr-info.ts
+++ src/pr-info.ts
@@ -484,10 +484,10 @@ function getReviews(prInfo: PR_repository_pullRequest) {
 }
 
 function getCIResult(checkSuites: PR_repository_pullRequest_commits_nodes_commit_checkSuites | null): { ciResult: CIResult, ciUrl?: string } {
-    const totalStatusChecks = checkSuites?.nodes?.find(check =>
-        check?.app?.name?.includes("GitHub Actions") &&
-        check.checkRuns?.nodes?.[0]?.title === "test"
-    );
+    const ghActionsChecks = checkSuites?.nodes?.filter(check => check?.app?.name?.includes("GitHub Actions"));
+    // Now that there is more than one GitHub Actions suite, we need to get the right one, but naively fall back
+    // to the first if we can't find it, mostly to prevent breaking old tests.
+    const totalStatusChecks = ghActionsChecks?.find(check => check?.checkRuns?.nodes?.[0]?.title === "test") || ghActionsChecks?.[0];
     if (!totalStatusChecks) return { ciResult: CIResult.Missing, ciUrl: undefined };
     switch (totalStatusChecks.conclusion) {
         case CheckConclusionState.SUCCESS:
