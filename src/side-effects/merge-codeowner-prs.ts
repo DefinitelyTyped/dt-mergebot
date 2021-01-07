8a1b244c9764f96444d541270cccb611f5bb6076
Eli Barzilay <eli@barzilay.org>
2021-01-07 10:29:28 -0500

    Convert `PR-Trigger/index.js` to TS
    * Removed `sideEffects` and instead just try it for the single entry
      that was there.
    
    * Refactored things a bit around `acceptedEventsToActions` and `allowListedActions`
    
    * Dropped `if (state.type === "noop")` (similar to #294)
    
    * Some touchups in `src/side-effects/merge-codeowner-prs`

--- src/side-effects/merge-codeowner-prs.ts
+++ src/side-effects/merge-codeowner-prs.ts
@@ -3,25 +3,21 @@ import { EventPayloads } from "@octokit/webhooks";
 import { createMutation, client } from "../graphql-client";
 
 export const mergeCodeOwnersOnGreen = async (payload: EventPayloads.WebhookPayloadCheckSuite) => {
-  // Because we only care about GH actions, we can use the check suite API which means we get both the
-  // commit and the PR, making it much less effort than other merge-on-greens
-  // https://github.com/microsoft/TypeScript-repos-automation/blob/40ae8b3db63fd0150938e82e47dcb63ce65f7a2d/src/checks/mergeOnGreen.ts#L1
+    // Because we only care about GH actions, we can use the check suite API which means we get both the
+    // commit and the PR, making it much less effort than other merge-on-greens
+    // https://github.com/microsoft/TypeScript-repos-automation/blob/40ae8b3db63fd0150938e82e47dcb63ce65f7a2d/src/checks/mergeOnGreen.ts#L1
 
-  if (payload.action !== "completed") return;
+    if (payload.action === "completed"
+        && payload.check_suite.conclusion === "success"
+        && payload.check_suite.head_commit.author.name === "TS Bot"
+        && payload.check_suite.head_commit.message === "Update CODEOWNERS"
+        && payload.check_suite.pull_requests[0].base.repo.id === payload.check_suite.pull_requests[0].head.repo.id) {
+        await client.mutate(createMutation<schema.MergePullRequestInput>("mergePullRequest", {
+            commitHeadline: `🤖 Auto Merge`,
+            expectedHeadOid: payload.check_suite.head_commit.id,
+            mergeMethod: "SQUASH",
+            pullRequestId: payload.check_suite.pull_requests[0].id.toFixed(),
+        }));
+    }
 
-  const isGreen = payload.check_suite.conclusion === "success";
-  const isFromBot = payload.check_suite.head_commit.author.name === "TS Bot";
-  const hasRightCommitMsg = payload.check_suite.head_commit.message === "Update CODEOWNERS";
-  const isFromSameRepo = payload.check_suite.pull_requests[0].base.repo.id === payload.check_suite.pull_requests[0].head.repo.id;
-
-  if (isGreen && isFromBot && hasRightCommitMsg && isFromSameRepo) {
-    const mergeMutation = createMutation<schema.MergePullRequestInput>("mergePullRequest", {
-      commitHeadline: `🤖 Auto Merge`,
-      expectedHeadOid: payload.check_suite.head_commit.id,
-      mergeMethod: "SQUASH",
-      pullRequestId: payload.check_suite.pull_requests[0].id.toFixed(),
-    });
-
-    await client.mutate(mergeMutation);
-  }
 };
