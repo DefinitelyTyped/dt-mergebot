3568e35d7496d8e6e0a4f698835b7321c5424d1f
Orta <git@orta.io>
2021-06-04 16:58:05 +0100

    Adds a general shape for discussions support

---
+++ src/_tests/discussions.test.ts
@@ -0,0 +1,14 @@
+import {canHandleRequest} from "../discussions-trigger"
+
+describe(canHandleRequest, () => {
+    const eventActions = [
+        ["discussion", "created", true],
+        ["discussion", "edited", true],
+        ["discussion", "updated", false],
+        ["pull_request", "created", false]
+    ] as const
+    
+    test.concurrent.each(eventActions)('(%s, %s) is %s', async (event, action, expected) => {
+        expect(canHandleRequest(event, action)).toEqual(expected);
+    });
+})
