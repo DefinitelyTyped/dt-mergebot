12706619298b586aea1c28c674e87b5686065e69
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-05-30 09:02:32 -0700

    try to get test environment passing CI

--- src/_tests/testEnvironment.js
+++ src/_tests/testEnvironment.js
@@ -3,8 +3,8 @@ const jestEnvNode = require("jest-environment-node");
 module.exports = class extends jestEnvNode.TestEnvironment {
     constructor(config) {
         super(config);
-        this.global.AbortSignal = AbortSignal;
-        this.global.Event = Event;
-        this.global.EventTarget = EventTarget;
+        this.global.AbortSignal = {};
+        this.global.Event = {};
+        this.global.EventTarget = {};
     }
 };
